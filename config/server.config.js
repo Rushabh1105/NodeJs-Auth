8770dca2b3b0b2ee6c4ed3df06b30c21ced82ccd
Rushabh Sakharkar <65095178+Rushabh1105@users.noreply.github.com>
2024-01-09 15:36:35 +0530

    added readme file

--- config/server.config.js
+++ config/server.config.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config()
 
 // Exporting required variables for use in project
-export const PORT = process.env.port;
+export const PORT = process.env.PORT;
 export const DB_URL = process.env.DB_URL;
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const USER = process.env.user;
